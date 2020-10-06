ddaf29c33343b978189e0e70947f3cecb0ceaffc
julianjohannesen <julian.johannesen@gmail.com>
2020-10-06 12:25:09 -0400

    form wrapper

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Form from "bulma/"
 
-default export function Form(props){
+default export function Form( {id,submitHandler,children} ) {
+    
     return(
-        
+        <form 
+            className="form" 
+            id={id}
+            onSubmit={submitHandler}
+        >
+            {children}
+        </form>
     );
 }
